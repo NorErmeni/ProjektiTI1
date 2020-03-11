614effe73497643824ff38619faed255da3fccfa
edontafilii <edon.tafili@riinvest.net>
2020-03-11 10:30:47 -0700

    dizejnimi i klasave te aplikacionit

---
+++ projektiTI1/Doktori.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace projektiTI1
+{
+    class Doktori:Personi
+    {
+        public string Pozita { get; set; }
+
+        public string Kualifikimi { get; set; }
+      
+
+        public override string ToString()
+        {
+            return "id: " + Id + "\nEmri: " + FirstName + "\nMbiemri: " + LasTname + "\nMosha: " + Age + "\nAdresa: " +
+                Address + "\nNumri telefonit: " + PhoneNumber + "\nPozita: " +
+                Pozita + "\nKualifikimi: " + Kualifikimi;
+
+        }
+    }
+}
---
+++ projektiTI1/Termini.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projektiTI1
+{
+    class Termini
+    {
+        public int Id { get; set; }
+        public Pacienti Pacient { get; set; }
+        public Doktori Doktor { get; set; }
+       
+        public DateTime Kohaeterminit { get; set; }
+
+        public override string ToString()
+        {
+            return "TerminId: " + Id + "\nPacient id: " + Pacient.Id + "\nDoktori id: " + Doktor.Id + "\nkoha e terminit: " + Kohaeterminit + "\nsherbimi id: " + Sherbimi.SherbimiId;
+        }
+    }
+}
